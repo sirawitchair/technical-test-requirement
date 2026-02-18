a7d31be8576e53957a891e69938beaa37ddaea12
sirawitpm <sirawit@liefcapital.com>
2026-02-18 16:59:36 +0700

    fix : admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
 from .models import User, UserProfile
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'created_at', 'updated_at')
-    list_filter = ('is_staff', 'is_active', 'created_at', 'updated_at')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_display = ('username', 'email', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('username', 'email')
     ordering = ('-created_at',)
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'first_name_th', 'last_name_th', 'first_name_en', 'last_name_en', 'created_at', 'updated_at')
+    list_display = ('user_id', 'first_name_th', 'last_name_th', 'first_name_en', 'last_name_en', 'created_at', 'updated_at')
     list_filter = ('created_at', 'updated_at')
-    search_fields = ('user__username', 'user__email', 'first_name_th', 'last_name_th', 'first_name_en', 'last_name_en')
+    search_fields = ('user_id__username', 'user_id__email', 'first_name_th', 'last_name_th', 'first_name_en', 'last_name_en')
     ordering = ('-created_at',)
